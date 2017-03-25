3c80f84c860c735d88ec81bbe3ca524e16602971
rachelwu11 <xianw5@gmail.com>
2017-03-25 00:55:47 -0400

    connect to mongodb

--- server.js
+++ server.js
@@ -4,20 +4,36 @@ const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('link-to-mongodb', (err, database) -> {
-	//... start server
+MongoClient.connect('mongodb://test1:a12345@ds117859.mlab.com:17859/star-wars-quotes', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	app.listen(3000, () => {
+ 		console.log('listening on 3000')
+	})
 })
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.listen(3000, function() {
-  console.log('listening on 3000')
-});
-app.get('/', (req, res) => {
- res.sendFile(__dirname + '/index.html')
- console.log(__dirname)
-})
+app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+//  res.sendFile(__dirname + '/index.html')
+//  console.log(__dirname)
+// })
 app.post('/quotes', (req, res) => {
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
 	console.log(req.body)
-	console.log('Hellllllllllloooooooo!')
 })
 
+app.get('/', (req, res) => {
+	db.collection('quotes').find().toArray(function(err, results) {
+		if (err) return console.log(err)
+		//renders index.ejs
+		res.render('index.ejs', {quotes: results})
+	console.log(results)
+	})
+})
